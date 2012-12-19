b0322edc67cd9e0cb254ab4b1630b369299cfb25
Dennis Rassmann <showp1984@gmail.com>
2012-12-19 11:14:11 +0100

    cpuinit: get rid of __cpuinit, first regexp
    This commit is the result of:
    
    find -name "*.c" | xargs sed -i "s/ __cpuinit / /g"
    
    Signed-off-by: Arjan van de Ven <arjan@linux.intel.com>
    Signed-off-by: Dennis Rassmann <showp1984@gmail.com>
    Signed-off-by: Zarboz <zarboz@gmail.com>
    Signed-off-by: Cerebro <zarboz@gmail.com>
    
    Conflicts:
    	arch/arm/kernel/smp.c
    	drivers/hwmon/coretemp.c

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -276,7 +276,7 @@ out:
 EXPORT_SYMBOL(cpu_down);
 #endif 
 
-static int __cpuinit _cpu_up(unsigned int cpu, int tasks_frozen)
+static int _cpu_up(unsigned int cpu, int tasks_frozen)
 {
 	int ret, nr_calls = 0;
 	void *hcpu = (void *)(long)cpu;
@@ -312,7 +312,7 @@ out_notify:
 }
 
 extern void trace_cpu_up_frequency (unsigned int cpu);
-int __cpuinit cpu_up(unsigned int cpu)
+int cpu_up(unsigned int cpu)
 {
 	int err = 0;
 
@@ -497,7 +497,7 @@ core_initcall(cpu_hotplug_pm_sync_init);
 
 #endif 
 
-void __cpuinit notify_cpu_starting(unsigned int cpu)
+void notify_cpu_starting(unsigned int cpu)
 {
 	unsigned long val = CPU_STARTING;
 
