0a455d449aab2b59003c78ccf17826e675a82f45
Lens-F <tnoah12@gmail.com>
2014-04-24 15:39:46 -0400

    aroma update [read more]
    Alot of edits in aroma with a lot of changes deletions updates and additions related to the new thermal and mpdecision solutions that were added and also the removal of deprecated governors.
    
    Signed-off-by: Lens-F <tnoah12@gmail.com>

---
+++ packages/boot/compatibility.sh
@@ -0,0 +1,12 @@
+#!/sbin/sh
+#
+
+#remove the binaries as they are no longer needed. (kernel handled)
+if [ -e /system/bin/mpdecision ] ; then
+	busybox mv /system/bin/mpdecision /system/bin/mpdecision_bck
+fi
+if [ -e /system/bin/thermal-engine-hh ] ; then
+	busybox mv /system/bin/thermal-engine-hh /system/bin/thermal-engine-hh_bck
+fi
+
+return $?
